3004ffc7f6385c0ea85a45a7eba0e0ca9d4000e3
sravan <sravan@sravans-macbook-pro.local>
2019-02-20 22:09:31 +0530

    Initial Commit

---
+++ DigiPass/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DigiPass
+//
+//  Created by sravan on 20/02/19.
+//  Copyright © 2019 ecknologic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
